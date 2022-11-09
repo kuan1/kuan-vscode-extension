5192ef3fc8bbd0eeeb07f41d0a3287eac7b5af04
luzhongkuan <luzhongkuan@xiaohongshu.com>
2022-11-09 11:14:14 +0800

    fix: 1

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,4 @@
-import {dirname} from 'path'
 import * as vscode from 'vscode'
-
-import { goods3Command, terminalMapping } from './goods3/goods3Command'
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('kuan-vscode activate')
@@ -11,41 +8,6 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage('Hello vscode World from kuan vscode!')
   })
   context.subscriptions.push(disposable);
-
-  // 商品3.0命令，循环依赖
-  disposable = vscode.commands.registerCommand('kuan-vscode.goods3', () => {
-    const filePath = vscode.window.activeTextEditor?.document.uri.fsPath
-    if (filePath) {
-      goods3Command(filePath)
-    } else {
-      vscode.window.showWarningMessage('没有找到活跃文件')
-    }
-  })
-  context.subscriptions.push(disposable);
-
-  // 指定目录修改
-  disposable = vscode.commands.registerCommand('kuan-vscode.goods3TransofromFolder', () => {
-    const filePath = vscode.window.activeTextEditor?.document.uri.fsPath
-    if (filePath) {
-      goods3Command(`-m 1 ${dirname(filePath)}`)
-    } else {
-      vscode.window.showWarningMessage('没有找到活跃文件')
-    }
-  })
-  context.subscriptions.push(disposable);
-
-  // 扫描模式
-  disposable = vscode.commands.registerCommand('kuan-vscode.goods3Dry', () => {
-    const filePath = vscode.window.activeTextEditor?.document.uri.fsPath
-    if (filePath) {
-      goods3Command(`-d ${filePath}`)
-    } else {
-      vscode.window.showWarningMessage('没有找到活跃文件')
-    }
-  })
-  context.subscriptions.push(disposable);
-
-  vscode.window.onDidCloseTerminal(term => terminalMapping.delete(term.name));
 }
 
 export function deactivate() {
